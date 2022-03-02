786782471af014417f5501e3dab68dd446c3489b
Fabio Alcocer Sejas <fabioalcocersejas17@gmail.com>
2022-03-02 17:06:20 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -14,22 +14,4 @@ headers.forEach(header => {
 
 
 
-const section = document.querySelector('section');
-
-section.addEventListener("click", function(e) {
-
-  const questions = Array.from(document.querySelectorAll(".faq__item"));
-  
-  for (let i = 0; i < questions.length; i++) {
-    var question = questions[i]
-    question.classList.remove('question--with-answer');
-  }
-
-  const currentQuestion = e.target;
-  currentQuestion.classList.toggle('question--with-answer')
-});
-
-
-
-
 
